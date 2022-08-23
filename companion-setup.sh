84e2cf6a62192c385ba7db2f584432fadc252e4d
Michele Fiorito <fiore.ansaya@gmail.com>
2022-08-24 01:08:16 +0200

    Companion setup script update

--- companion-setup.sh
+++ companion-setup.sh
@@ -10,12 +10,13 @@ scripts_dir="${script_dir}/scripts"
 
 function install_dependencies () {
     sudo apt update
-    sudo apt install -y autossh g++ gcc git help2man libgirepository1.0-dev \
-        libgudev-1.0-dev meson ninja-build pkg-config udhcpd ufw
+    sudo apt install -y autossh g++ gcc git haveged help2man \
+        libgirepository1.0-dev libgudev-1.0-dev meson ninja-build \
+        pkg-config ufw
 }
 
 function setup_autossh_service () {
-sudo cat > /etc/systemd/system/$1 << EOF
+sudo dd of=/etc/systemd/system/$1 << EOF
 [Unit]
 Description=Reverse proxy service
 After=network-online.target
@@ -36,14 +37,12 @@ install_dependencies
 
 ${scripts_dir}/mavlink-install.sh
 
-${scripts_dir}/libqmi-install.sh
-
 echo " => Updating global environment variables..."
 cat env | sudo tee -a /etc/environment
 source /etc/environment
 
 echo " => Generating MAVlink router configuration..."
-sudo cat > /etc/mavlink-router/main.conf << EOF
+sudo dd of=/etc/mavlink-router/main.conf << EOF
 [General]
 ReportStats = true
 DebugLogLevel = info
@@ -59,56 +58,27 @@ FlowControl = false
 EOF
 
 echo " => Reverse SSH automated connection service configuration..."
-setup_autossh_service remote-mavlink.service $MAV_PORT $RELAY_IP $MAV_PORT
-setup_autossh_service remote-ssh.service 22 $RELAY_IP $MAV_SSH
+setup_autossh_service remote-mavlink.service "\$MAV_PORT" "\$RELAY_IP" "\$MAV_PORT"
+setup_autossh_service remote-ssh.service 22 "\$RELAY_IP" "\$MAV_SSH"
+
+echo " => Setup Network Manager"
+sudo ${scripts_dir}/switch_to_networkmanager.sh
 
 echo " => Airspace Access Point setup..."
 ap_companion_ssid="DroneAirspace"
 ap_companion_pass="Dron3Airsp@c3"
-ap_companion_ip="192.168.100.254"
-sudo cat > /etc/udhcpd.conf << EOF
-start           $(sed -E 's/.[0-9]+$//g' <<< ${ap_companion_ip}).1
-end             $(sed -E 's/.[0-9]+$//g' <<< ${ap_companion_ip}).5
-interface       wlan0
-remaining       yes
-opt     dns     ${ap_companion_ip}
-opt     subnet  255.255.255.0
-opt     router  ${ap_companion_ip}
-opt     lease   1800
-EOF
-sudo sed -i 's/DHCPD_ENABLED="no"/DHCPD_ENABLED="yes"/' /etc/default/udhcpd
-sudo cat > /etc/network/interfaces.d/airspace << EOF
-iface wlan0 inet static
-address ${ap_companion_ip}
-netmask 255.255.255.0
-wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
-EOF
-sudo cat > /etc/hostapd/hostapd.conf << EOF
-interface=wlan0
-driver=n180211
-ssid=${ap_companion_ssid}
-hw_mode=g
-channel=6
-macaddr_ac1=0
-auth_algs=1
-ignore_broadcast_ssid=0
-ieee80211n=1
-dtim_period=1
-max_num_sta=8
-wpa=2
-wpa_key_mgmt=SAE
-rsn_pairwise=CCMP
-ieee80211w=2
-wpa_passphrase=$(wpa_passphrase ${ap_companion_ssid} ${ap_companion_pass} | grep psk= | tail -1 | sed 's/.*=//g')
-sae_pwe=2
-EOF
-echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' | sudo tee -a /etc/default/hostapd
+ap_companion_ip="192.168.100.128"
+echo "    Static IP is ${ap_companion_ip}"
+echo "    Access point SSID is ${ap_companion_ssid}"
+echo "    Access point password is ${ap_companion_pass}"
+sudo ${scripts_dir}/nm_ap_setup.sh wlan0 "${ap_companion_ssid}" "${ap_companion_pass}" "${ap_companion_ip}"
+
 sudo ufw allow from $(sed -E 's/.[0-9]+$//g' <<< ${ap_companion_ip}).0/24
 sudo ufw allow SSH
 echo y | sudo ufw enable
 
 sudo systemctl daemon-reload
-for service in "mavlink-router" "remote-mavlink" "remote-ssh" "hostapd" "udhcpd"
+for service in "mavlink-router" "remote-mavlink" "remote-ssh" "haveged"
 do
     sudo systemctl enable ${service}
     sudo systemctl start ${service}
